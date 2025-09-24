b0080b16118fa6d5adfdc56ed91adf78a69b53aa
aaronwzq <aaron.quek.2024@computing.smu.edu.sg>
2025-09-24 03:22:37 +0000

    commision

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,18 +1,43 @@
 <script>
     export default { 
         // Add Code Here
-        // challenge: use array  
+        // challenge: use array
+        data()
+        {
+            return {
+                message: "Choose your favourite fruit:",
+                fruit1: "apple",
+                fruit2: "orange",
+                fruit3: "peach",
+
+                // use array
+                fruits: ["apple", "orange", "peach"]
+            }
+        }
+
     }
 </script>
 
 <template>
     <div id="app">
         {{ message }} </br>
-        {{ fruit1 }} <input type="radio" name="fruit">
+        {{ fruit1 }} <input type="radio" name="fruits">
         {{ fruit2 }} <input type="radio" name="fruit">
         {{ fruit3 }} <input type="radio" name="fruit">
+
+        <h3>Using Array</h3>
+        {{ message }} </input> </br>
+
+        <span v-for="fruit in fruits">
+            {{ fruit }} <input type="radio" name="fruits" class="radio">&nbsp;
+        </span>
     </div>
 </template>
 
 <style scoped>
+
+.radio
+{
+    background-color: white;
+}
 </style>
--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -13,5 +13,6 @@
 <template>
 
     <!-- add code here -->
+     <img v-bind:src="myImg" v-bind:width="width" v-bind:height="height"></img>
    
 </template>
